9dbad507e4b1b2dcbe019a0b22704cb8c322f741
Pranjal Mantri <pranjalmantri@gmail.com>
2025-07-20 15:44:52 +0530

    feat: implement user model with password hashing and comparison methods

---
+++ backend/src/model/user.model.ts
@@ -0,0 +1,74 @@
+import mongoose, { Document, Schema } from "mongoose";
+import bcrypt from "bcrypt";
+
+interface UserInput {
+  email: string;
+  password: string;
+  fullname: string;
+  profilePicture?: string;
+  isAdmin: boolean;
+  points: number;
+}
+
+interface UserDocument extends UserInput, Document {
+  _id: string;
+  createdAt: Date;
+  updatedAt: Date;
+  comparePassword: (candidatePassword: string) => Promise<boolean>;
+}
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePicture: {
+      type: String,
+    },
+    points: {
+      type: Number,
+      default: 0,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre<UserDocument>("save", async function (this: UserDocument, next) {
+  const user = this;
+
+  if (!user.isModified("password")) return next();
+
+  const salt = await bcrypt.genSalt(Number(process.env.SALT_FACTOR));
+  const hashedPassword = bcrypt.hashSync(user.password, salt);
+
+  user.password = hashedPassword;
+  next();
+});
+
+userSchema.methods.comparePassword = async function (
+  this: UserDocument,
+  candidatePassword: string
+): Promise<boolean> {
+  const user = this;
+
+  return bcrypt.compare(candidatePassword, user.password).catch((e) => false);
+};
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
