60c0b1cd7e342c41bbf210e12b61cebe6f460c18
Pranjal Mantri <pranjalmantri@gmail.com>
2025-07-20 21:12:47 +0530

    feat: add cookie-parser dependency and middleware for request handling

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,13 @@
 import express, { NextFunction, Request, Response } from "express";
 import dotenv from "dotenv";
 import connectDb from "./db/connectDb.ts";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 const port = process.env.PORT || 3000;
 
