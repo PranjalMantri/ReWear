aa994f3d7be5a0a70b7a9a81e1ccfea94eb78b0b
Pranjal Mantri <pranjalmantri@gmail.com>
2025-07-20 13:13:36 +0530

    feat: wrap HomePage in Layout component to include Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import SignupPage from "./pages/SignupPage";
 import SigninPage from "./pages/SigninPage";
 import useUserStore from "./store/user.store";
 import HomePage from "./pages/HomePage";
+import Layout from "./pages/Layout";
 
 function App() {
   const isUserLoggedIn = useUserStore((state) => state.isUserLoggedIn);
@@ -11,7 +12,18 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={isUserLoggedIn ? <HomePage /> : <Landing />} />
+        <Route
+          path="/"
+          element={
+            isUserLoggedIn ? (
+              <Layout>
+                <HomePage />{" "}
+              </Layout>
+            ) : (
+              <Landing />
+            )
+          }
+        />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/signin" element={<SigninPage />} />
       </Routes>
