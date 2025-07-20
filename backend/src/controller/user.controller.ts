e2f34bb708f7dca2027f56ddc20a54630180d898
Pranjal Mantri <pranjalmantri@gmail.com>
2025-07-20 19:41:18 +0530

    feat: add signin functionality with validation and token generation

--- backend/src/controller/user.controller.ts
+++ backend/src/controller/user.controller.ts
@@ -8,7 +8,10 @@ import { asyncHandler } from "../util/asyncHandler.ts";
 
 import { createTokens } from "../util/jwt.ts";
 
-import { signupSchema } from "../../../common/schema/user.schema.ts";
+import {
+  signinSchema,
+  signupSchema,
+} from "../../../common/schema/user.schema.ts";
 
 const signup = asyncHandler(async (req: Request, res: Response) => {
   const validatedData = signupSchema.safeParse(req.body);
@@ -64,4 +67,60 @@ const signup = asyncHandler(async (req: Request, res: Response) => {
     .json(new ApiResponse(201, "User registered succcessfuly", userResponse));
 });
 
-export { signup };
+const signin = asyncHandler(async (req: Request, res: Response) => {
+  const validatedData = signinSchema.safeParse(req.body);
+
+  if (!validatedData.success) {
+    throw new ApiError(
+      400,
+      "Invalid input",
+      validatedData.error.flatten().fieldErrors
+    );
+  }
+
+  const { email, password } = validatedData.data!;
+
+  const existingUser = await User.findOne({ email });
+
+  if (!existingUser) {
+    throw new ApiError(404, "User does not exist");
+  }
+
+  const isPasswordValid = await existingUser.isPasswordValid(password);
+
+  if (!isPasswordValid) {
+    throw new ApiError(401, "Invalid user credentials");
+  }
+
+  const { accessToken, refreshToken } = createTokens({
+    userId: existingUser._id,
+  });
+
+  const cookieOptions = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV == "production",
+    sameSite: true,
+  };
+
+  res.cookie("accessToken", accessToken, {
+    ...cookieOptions,
+    maxAge: 15 * 60 * 1000,
+  });
+
+  res.cookie("refreshToken", refreshToken, {
+    ...cookieOptions,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
+
+  const userResponse = {
+    id: existingUser._id,
+    fullname: existingUser.fullname,
+    email: existingUser.email,
+  };
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, "User signup succcessfull", userResponse));
+});
+
+export { signup, signin };
