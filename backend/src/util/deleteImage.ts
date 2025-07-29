927a831a342fd7064db268ee77a493874d4f43ec
Pranjal Mantri <pranjalmantri@gmail.com>
2025-07-29 22:45:16 +0530

    feat: add deleteImage utility to handle image deletion from Cloudinary

---
+++ backend/src/util/deleteImage.ts
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from "cloudinary";
+
+function extractPublicIdFromUrl(url: string): string {
+  const parts = url.split("/");
+  const fileName = parts[parts.length - 1];
+  const publicId = fileName.split(".")[0];
+
+  return `item-images/${publicId}`;
+}
+
+export const deleteImage = async (imageUrl: string) => {
+  try {
+    const publicId = extractPublicIdFromUrl(imageUrl);
+
+    const response = await cloudinary.uploader.destroy(publicId);
+
+    if (response.result == "ok" || response.result == "not found") {
+      return true;
+    }
+
+    console.log("Image deletion failed: ", response);
+    return false;
+  } catch (error) {
+    console.log("Error deleting image: ", error);
+    return false;
+  }
+};
