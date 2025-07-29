a8b0cf11436f7137e65a71e55cb1321223358e9e
Pranjal Mantri <pranjalmantri@gmail.com>
2025-07-30 00:56:27 +0530

    feat: add Swap model and schema for item swapping functionality

---
+++ backend/src/model/swaps.model.ts
@@ -0,0 +1,52 @@
+import mongoose, { Date, Document, Schema } from "mongoose";
+import {
+  SwapInputSchema,
+  SwapStatusEnum,
+} from "../../../common/schema/swap.schema.ts";
+import { z } from "zod";
+
+type SwapInput = z.infer<typeof SwapInputSchema>;
+
+interface SwapDocument extends SwapInput, Document {
+  _id: mongoose.Types.ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const swapSchema = new Schema(
+  {
+    proposer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    proposedItemId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Item",
+      required: true,
+    },
+    receiver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiveItemId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Item",
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: SwapStatusEnum.options,
+      default: "pending",
+    },
+    message: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const Swap = mongoose.model<SwapDocument>("Swap", swapSchema);
+
+export default Swap;
---
+++ common/schema/swap.schema.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+export const SwapStatusEnum = z.enum([
+  "pending",
+  "accepted",
+  "rejected",
+  "cancelled",
+  "completed",
+]);
+
+export const SwapInputSchema = z.object({
+  proposer: z.string().trim().min(1, "Swap proposer Id is required"),
+  proposedItemId: z.string().trim().min(1, "Proposed item id is required"),
+  receiver: z.string().min(1, "Swap receiver Id is required"),
+  receiveItemId: z.string().trim().min(1, "receiver item id is required"),
+  status: SwapStatusEnum.default("pending"),
+  message: z.string().optional(),
+});
