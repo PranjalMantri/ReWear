75b98f9c37d54a289aed1dc9fa752f5f8ee27ac6
Pranjal Mantri <pranjalmantri@gmail.com>
2025-08-11 13:23:15 +0530

    feat: add response interceptor to handle token refresh for API requests

--- frontend/src/util/api.ts
+++ frontend/src/util/api.ts
@@ -9,4 +9,43 @@ const api = axios.create({
   withCredentials: true,
 });
 
+const refreshApi = axios.create({
+  baseURL: "http://localhost:3000/api/v1",
+  withCredentials: true,
+});
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+
+    const skipRefreshEndpoints = [
+      "/user/signin",
+      "/user/signup",
+      "/user/refresh",
+    ];
+
+    if (skipRefreshEndpoints.some((url) => originalRequest.url.includes(url))) {
+      return Promise.reject(error);
+    }
+
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        const response = await refreshApi.post("/user/refresh");
+
+        console.log(response);
+        return api(originalRequest);
+      } catch (err) {
+        console.error("Refresh token failed:", err);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
 export default api;
