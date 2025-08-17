2c27f64e9d08738fd5748957fb7127257c3a8d3a
Pranjal Mantri <pranjalmantri@gmail.com>
2025-08-17 16:51:44 +0530

    feat: add user schema

--- common/schema/user.schema.ts
+++ common/schema/user.schema.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-const signupSchema = z
+export const signupSchema = z
   .object({
     email: z.string().email("Please enter a valid email address."),
     fullname: z.string().min(3, "Fullname must be at least 3 characters long."),
@@ -12,11 +12,16 @@ const signupSchema = z
     message: "Passwords do not match.",
   });
 
-const signinSchema = z.object({
+export const signinSchema = z.object({
   email: z.email("Invalid Email"),
   password: z
     .string("Password is required")
     .min(6, "Password must be at least 6 characters long"),
 });
 
-export { signupSchema, signinSchema };
+export const user = z.object({
+  email: z.string(),
+  fullname: z.string(),
+  profilePicture: z.string().optional(),
+  points: z.number(),
+});
