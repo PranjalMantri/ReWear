751128f4858f019beb0f83781680380eca3928ec
Pranjal Mantri <pranjalmantri@gmail.com>
2025-08-27 18:41:15 +0530

    refactor: remove Content-Type header from API configuration

--- frontend/src/util/api.ts
+++ frontend/src/util/api.ts
@@ -3,9 +3,6 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "http://localhost:3000/api/v1",
   timeout: 5000,
-  headers: {
-    "Content-Type": "application/json",
-  },
   withCredentials: true,
 });
 
